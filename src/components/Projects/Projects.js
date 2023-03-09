cacf346833adaf5cc0f51188a97c263cc2bdf8f9
Mahmoud Fouad <mfapolice@gmail.com>
2023-03-09 06:42:53 +0200

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-
 
 import { getDocs, collection } from "firebase/firestore/lite";
 import { db } from "../../firebase";
@@ -18,32 +17,6 @@ function Projects() {
     const querySnapshot = await getDocs(collection(db, "portfolio"));
     setPortfolio(querySnapshot.docs.map((doc) => doc.data()));
   };
-  console.log(portfolio);
-
-  const renderPortfolio = (portfolio) => {
-    return (
-    <>
-        {portfolio.map((port, idx) => {
-          const projectData = { ...port, id: idx };
-          console.log(projectData);
-          return (
-
-              <Col md={4} className="project-card">
-                <ProjectCard
-                  imgPath={projectData.image}
-                  isBlog={false}
-                  title={projectData.description}
-                  description={projectData.description}
-                  ghLink={projectData.github}
-                  demoLink={projectData.url}
-                />
-              </Col>
-          );
-        })}
-</>
-    );
-  };
-
   return (
     <Container fluid className="project-section">
       <Particle />
@@ -52,10 +25,21 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are the latest projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-            {renderPortfolio(portfolio)}
+          {portfolio.map((port, idx) => (
+            <Col key={idx} md={4} className="project-card">
+              <ProjectCard
+                imgPath={port.image}
+                isBlog={false}
+                title={port.description}
+                description={port.description}
+                ghLink={port.github}
+                demoLink={port.url}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </Container>
